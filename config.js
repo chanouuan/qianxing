a4ca8101f80cd9ec5c2acc92ef4a234b5bb11008
changyouquan <492441479@qq.com>
2020-05-11 17:50:57 +0800

    1

--- config.js
+++
@@ -1,5 +0,0 @@
-export const baseURL = 'https://server.hqians.com';
-//export const baseURL = 'http://192.168.0.102:85';
-export const version = '1.0.0';
-export const amapKey = '2a877428258065644fef06ed3e24e1f5';
-export const qqmapKey = 'RJABZ-M7ZWS-MVAOZ-6X3DM-27WKO-D4F5G';
